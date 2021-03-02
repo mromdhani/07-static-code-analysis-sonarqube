07c584bb0f0dac258322f52f4474af90bb5c175c
Mohamed Romdhani <m.romdhani@businesstraining.be>
2021-03-02 23:19:01 +0100

    adding untested code

--- src/main/java/be/businesstraining/domain/Message.java
+++ src/main/java/be/businesstraining/domain/Message.java
@@ -4,4 +4,47 @@ public class Message {
 	public String sayHello() {
 		return "Hello World";
 	}
+	public int adder1() {
+		// Some untested code
+	    int i =5;
+		int j =5;
+		return i+j;
+     }
+	
+	public int adder2() {
+		// Some untested code
+	    int i =5;
+		int j =5;
+		return i+j;
+     }
+	 public int adder3() {
+		// Some untested code
+	    int i =5;
+		int j =5;
+		return i+j;
+     }
+	 public int adder4() {
+		// Some untested code
+	    int i =5;
+		int j =5;
+		return i+j;
+     }
+	 public int adder5() {
+		// Some untested code
+	    int i =5;
+		int j =5;
+		return i+j;
+     }
+	 public int adder6() {
+		// Some untested code
+	    int i =5;
+		int j =5;
+		return i+j;
+     }
+	 public int adder7() {
+		// Some untested code
+	    int i =5;
+		int j =5;
+		return i+j;
+     }
 }
